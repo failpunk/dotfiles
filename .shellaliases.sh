3cde34ca004695625ce7ef0d0712d983d34cdccf
Justin Vencel <failpunk@gmail.com>
2013-01-09 12:20:47 -0800

    Update .shellaliases.sh

--- .shellaliases.sh
+++ .shellaliases.sh
@@ -11,10 +11,16 @@ alias json='python -mjson.tool'
 alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
-alias gd='git diff'
+alias gd='git diff --word-diff'
 alias gc='git commit'
 alias gca='git commit -a'
 alias gco='git checkout'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
+alias gpull='git pull origin'
+alias greset='git reset --hard HEAD | git clean -fd'
+
+# terminal color
+export CLICOLOR=1
+export LSCOLORS=GxFxCxDxBxegedabagaced
